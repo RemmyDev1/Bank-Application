4225e4a1357a672c67008c434d16f1640b2c6723
RemmyDev1 <85520351+RemmyDev1@users.noreply.github.com>
2024-05-10 20:41:05 -0400

    Update Bank-Manager-System.cpp

--- Bank-Manager-System.cpp
+++ Bank-Manager-System.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <conio.h>
+#include <Windows.h>
+#include <math.h>
+using namespace std;
 
+void EncryptData()
+{
+
+  
+}
+
+struct MinorityUsers
+{
+    string Fname = "";
+    string Lname = "";
+    int SSN = 0;
+    //SA = Savings account!
+    boolean SA = false;
+    // AU = Underadged account!
+    boolean UA = false;
+    int PhoneNum = 0;
+    int Money = 0;
+    int IncomePerYear = 0;
+    int LWP = 0;
+    int LMP = 0;
+    int LYP = 0;
+    int ATP = 0;
+    // Has a credit card?
+    boolean HCC = false;
+    // Always starts a 650
+    int CCS = 650;
+    // This is always the starter after income is determined 
+and calculated if payment history exists (CCL) Credt Card Limit will change
+    int CCL = 500;
+    // Has Debit card?
+    boolean HDC = false;
+    int MIDC = 0;
+
+    
+}
