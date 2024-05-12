d9ee7a7fe2c1bf7d90e953b3f385bfa01e3c3536
RemmyDev <85520351+RemmyDev1@users.noreply.github.com>
2024-05-12 14:06:50 -0400

    Update Bank-Application.cpp

--- Bank-Application.cpp
+++ Bank-Application.cpp
@@ -9,7 +9,37 @@
 
 using namespace std;
 
+void UserInfo()
+{
+    cout << "Your Information"
+}
+
+
 void App()
 {
+    int choice = 0;
     
+    cout << "\t\tBank Application" << endl;
+    cout << "1.View your Information" << endl;
+    cout << "2.Card Status" << endl;
+    cout << "3.Take Out Loan" << endl;
+    cout << "4.deposit Money Into Savings Account From Debt Card"
+    
+    cin >> choice; 
+    
+    switch(choice)
+    {
+        case 1:
+        {
+            UserInfo();
+        }
+        case 2:
+        {
+            CardStatus();
+        }
+        case 3:
+        {
+            LoanDep();
+        }
+    }
 }
